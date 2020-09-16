93c20b08f90adbab1cfac39894592ee031422a78
Simon Mutch <smutch@unimelb.edu.au>
2020-09-17 09:09:01 +1000

    Fix type of `theta`

--- src/smap_block_mdap.c
+++ src/smap_block_mdap.c
@@ -459,7 +459,7 @@ static ST_retcode mf_smap_single(ST_int rowsm, ST_int colsm, ST_double** M, ST_d
 /* OpenMP routines */
 
 ST_retcode mf_smap_loop(ST_int count_predict_set, ST_int count_train_set, ST_double** Bi_map, ST_int mani,
-                        ST_double** M, ST_double** Mp, ST_double* y, ST_int l, ST_int theta, ST_double* S,
+                        ST_double** M, ST_double** Mp, ST_double* y, ST_int l, ST_double theta, ST_double* S,
                         char* algorithm, ST_int save_mode, ST_int varssv, ST_int force_compute, ST_int missingdistance,
                         ST_double* ystar)
 {
