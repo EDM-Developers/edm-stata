1c6af7a9072fb40f7a755960a1eed87b064627c8
Edoardo Tescari <etescari@unimelb.edu.au>
2020-10-14 19:32:09 +1100

    updated minindex to correctly handle edge case where library = e + 2

--- src/edm.c
+++ src/edm.c
@@ -108,7 +108,8 @@ static int minindex(int rvect, double vect[], int k, int ind[])
       }
     }
   }
-
+  if (k == rvect) numind++;
+  
   /* returning the number of k minimums (and indices via ind) */
   return numind;
 }
