7d18406d2596481f82f777cd182333c4a78c3ba8
Pat <patrick.laub@gmail.com>
2021-06-21 15:22:01 +1000

    Adding some function comments to edm.cpp & better variable names

--- src/edm.cpp
+++ src/edm.cpp
@@ -23,20 +23,39 @@
 #include <cmath>
 #include <numeric> // std::iota
 
-/* minindex(v,k) returns the indices of the k minimums of v.  */
-std::vector<size_t> minindex(const std::vector<double>& v, int k)
+// For a given point, find the k nearest neighbours of this point.
+//
+// We are given the distances to all potential neighbours as a vector.
+// We return the 'k' indices of the neighbours.
+//
+// The distance vector may contain missing values which are coded as a massive
+// double value with the MISSING macro; this should be enough to put those
+// neighbours last in after sorting by closeness.
+//
+// If there are many potential neighbours with the exact same distances, we
+// prefer the neighbours with the smallest index value. This corresponds
+// to a stable sort in C++ STL terminology.
+//
+// In typical use-cases of 'edm explore' the value of 'k' is small, like 5-20.
+// However for a typical 'edm xmap' the value of 'k' is set as large as possible.
+// If 'k' is small, the partial_sort is efficient as it only finds the 'k' smallest
+// distances. If 'k' is larger, then it is faster to simply sort the entire distance
+// vector.
+//
+// N.B. The equivalent function in Stata/Mata is called 'minindex'.
+std::vector<size_t> kNearestNeighboursIndices(const std::vector<double>& dists, int k)
 {
   // Initialize original index locations
-  std::vector<size_t> idx(v.size());
+  std::vector<size_t> idx(dists.size());
   std::iota(idx.begin(), idx.end(), 0);
 
-  if (k >= (int)(v.size() / 2)) {
-    auto comparator = [&v](size_t i1, size_t i2) { return v[i1] < v[i2]; };
+  if (k >= (int)(dists.size() / 2)) {
+    auto comparator = [&dists](size_t i1, size_t i2) { return dists[i1] < dists[i2]; };
     std::stable_sort(idx.begin(), idx.end(), comparator);
   } else {
-    auto stableComparator = [&v](size_t i1, size_t i2) {
-      if (v[i1] != v[i2])
-        return v[i1] < v[i2];
+    auto stableComparator = [&dists](size_t i1, size_t i2) {
+      if (dists[i1] != dists[i2])
+        return dists[i1] < dists[i2];
       else
         return i1 < i2;
     };
@@ -52,6 +71,23 @@ void smap_prediction(int Mp_i, int t, Options opts, const Manifold& M, const Man
                      const std::vector<double>& d, const std::vector<size_t>& ind, span_2d_double ystar,
                      span_2d_double coeffs, span_2d_retcode rc);
 
+// Use a training manifold 'M' to make a prediction about the prediction manifold 'Mp'.
+// Specifically, predict the 'Mp_i'-th value of the prediction manifold 'Mp'.
+//
+// The predicted value is stored in 'ystar', along with any return codes in 'rc'.
+// Optionally, the user may ask to store some S-map intermediate values in 'coeffs'.
+//
+// The 'opts' value specifies the kind of prediction to make (e.g. S-map, or simplex method).
+// This function is usually run in a worker thread, and the 'keep_going' callback is frequently called to
+// see whether the user still wants this result, or if they have given up & simply want the execution
+// to terminate.
+//
+// We sometimes let 'M' and 'Mp' be the same manifold, so we train and predict using the same values.
+// In this case, the algorithm may cheat by pulling out the identical trajectory from the training manifold
+// and using this as the prediction. The 'skipRow' variable, when positive, indicates that we skip the
+// closest neighbour as it is probably cheating. 'skipRow' actually stores the specific index
+// of the training manifold which should not be used for this prediction, and in the future we will
+// use this directly, though some details need to be worked out.
 void make_prediction(int Mp_i, Options opts, const Manifold& M, const Manifold& Mp, span_2d_double ystar,
                      span_2d_retcode rc, span_2d_double coeffs, int skipRow, bool keep_going() = nullptr)
 {
@@ -59,7 +95,7 @@ void make_prediction(int Mp_i, Options opts, const Manifold& M, const Manifold& 
     return;
   }
   int validDistances = 0;
-  std::vector<double> d(M.nobs());
+  std::vector<double> dists(M.nobs());
 
   for (int i = 0; i < M.nobs(); i++) {
     double dist = 0.;
@@ -82,10 +118,10 @@ void make_prediction(int Mp_i, Options opts, const Manifold& M, const Manifold& 
     dist += numMissingDims * opts.missingdistance * opts.missingdistance;
 
     if (!missing) {
-      d[i] = sqrt(dist);
+      dists[i] = sqrt(dist);
       validDistances += 1;
     } else {
-      d[i] = MISSING;
+      dists[i] = MISSING;
     }
   }
 
@@ -113,19 +149,19 @@ void make_prediction(int Mp_i, Options opts, const Manifold& M, const Manifold& 
     }
   }
 
-  double d_min = *std::min_element(d.begin(), d.end());
-  bool skipFirst = (d_min > 0) && (skipRow >= 0);
+  double minDist = *std::min_element(dists.begin(), dists.end());
+  bool skipFirst = (minDist > 0) && (skipRow >= 0);
 
-  for (int i = 0; i < d.size(); i++) {
-    if (d[i] == 0) {
-      d[i] = MISSING;
+  for (int i = 0; i < dists.size(); i++) {
+    if (dists[i] == 0) {
+      dists[i] = MISSING;
     }
   }
 
-  std::vector<size_t> ind = minindex(d, k + skipFirst);
+  std::vector<size_t> kNNInds = kNearestNeighboursIndices(dists, k + skipFirst);
 
   if (skipFirst) {
-    ind.erase(ind.begin(), ind.begin() + 1);
+    kNNInds.erase(kNNInds.begin(), kNNInds.begin() + 1);
   }
 
   if (keep_going != nullptr && keep_going() == false) {
@@ -134,7 +170,7 @@ void make_prediction(int Mp_i, Options opts, const Manifold& M, const Manifold& 
 
   if (opts.algorithm == "" || opts.algorithm == "simplex") {
     for (int t = 0; t < opts.thetas.size(); t++) {
-      simplex_prediction(Mp_i, t, opts, M, k, d, ind, ystar, rc);
+      simplex_prediction(Mp_i, t, opts, M, k, dists, kNNInds, ystar, rc);
     }
   } else if (opts.algorithm == "smap" || opts.algorithm == "llr") {
     bool saveCoeffsForLargestTheta = opts.saveSMAPCoeffs;
@@ -143,7 +179,7 @@ void make_prediction(int Mp_i, Options opts, const Manifold& M, const Manifold& 
       if (t == opts.thetas.size() - 1) {
         opts.saveSMAPCoeffs = saveCoeffsForLargestTheta;
       }
-      smap_prediction(Mp_i, t, opts, M, Mp, k, d, ind, ystar, coeffs, rc);
+      smap_prediction(Mp_i, t, opts, M, Mp, k, dists, kNNInds, ystar, coeffs, rc);
     }
   } else {
     for (int t = 0; t < opts.thetas.size(); t++) {
@@ -152,18 +188,18 @@ void make_prediction(int Mp_i, Options opts, const Manifold& M, const Manifold& 
   }
 }
 
-void simplex_prediction(int Mp_i, int t, Options opts, const Manifold& M, int k, const std::vector<double>& d,
-                        const std::vector<size_t>& ind, span_2d_double ystar, span_2d_retcode rc)
+void simplex_prediction(int Mp_i, int t, Options opts, const Manifold& M, int k, const std::vector<double>& dists,
+                        const std::vector<size_t>& kNNInds, span_2d_double ystar, span_2d_retcode rc)
 {
   double theta = opts.thetas[t];
 
-  double d_base = d[ind[0]];
+  double d_base = dists[kNNInds[0]];
   std::vector<double> w(k);
   double sumw = 0., r = 0.;
 
   for (int j = 0; j < k; j++) {
-    if (d[ind[j]] != MISSING) {
-      w[j] = exp(-theta * (d[ind[j]] / d_base));
+    if (dists[kNNInds[j]] != MISSING) {
+      w[j] = exp(-theta * (dists[kNNInds[j]] / d_base));
     } else {
       w[j] = 0;
     }
@@ -171,7 +207,7 @@ void simplex_prediction(int Mp_i, int t, Options opts, const Manifold& M, int k,
   }
 
   for (int j = 0; j < k; j++) {
-    r = r + M.y(ind[j]) * (w[j] / sumw);
+    r = r + M.y(kNNInds[j]) * (w[j] / sumw);
   }
 
   ystar(t, Mp_i) = r;
@@ -179,7 +215,7 @@ void simplex_prediction(int Mp_i, int t, Options opts, const Manifold& M, int k,
 }
 
 void smap_prediction(int Mp_i, int t, Options opts, const Manifold& M, const Manifold& Mp, int k,
-                     const std::vector<double>& d, const std::vector<size_t>& ind, span_2d_double ystar,
+                     const std::vector<double>& dists, const std::vector<size_t>& kNNInds, span_2d_double ystar,
                      span_2d_double coeffs, span_2d_retcode rc)
 {
   std::vector<double> w(k);
@@ -189,8 +225,8 @@ void smap_prediction(int Mp_i, int t, Options opts, const Manifold& M, const Man
   double mean_d = 0.;
   int kValid = 0;
   for (int j = 0; j < k; j++) {
-    if (d[ind[j]] != MISSING) {
-      mean_d = mean_d + d[ind[j]];
+    if (dists[kNNInds[j]] != MISSING) {
+      mean_d = mean_d + dists[kNNInds[j]];
       kValid += 1;
     }
   }
@@ -201,8 +237,8 @@ void smap_prediction(int Mp_i, int t, Options opts, const Manifold& M, const Man
   // Need to check for missing values because e^(-theta*w[j]) = e^(-0 * MISSING) = 1
   // will still gives weight to missing values.
   for (int j = 0; j < k; j++) {
-    if (d[ind[j]] != MISSING) {
-      w[j] = exp(-theta * (d[ind[j]] / mean_d));
+    if (dists[kNNInds[j]] != MISSING) {
+      w[j] = exp(-theta * (dists[kNNInds[j]] / mean_d));
     } else {
       w[j] = 0;
     }
@@ -210,7 +246,7 @@ void smap_prediction(int Mp_i, int t, Options opts, const Manifold& M, const Man
 
   int rowc = -1;
   for (int j = 0; j < k; j++) {
-    if (M.any_missing(ind[j])) {
+    if (M.any_missing(kNNInds[j])) {
       continue;
     }
     rowc++;
@@ -220,10 +256,10 @@ void smap_prediction(int Mp_i, int t, Options opts, const Manifold& M, const Man
       return;
 
     } else if (opts.algorithm == "smap") {
-      y_ls[rowc] = M.y(ind[j]) * w[j];
+      y_ls[rowc] = M.y(kNNInds[j]) * w[j];
       w_ls[rowc] = w[j];
       for (int i = 0; i < M.E_actual(); i++) {
-        X_ls(rowc, i) = M(ind[j], i) * w[j];
+        X_ls(rowc, i) = M(kNNInds[j], i) * w[j];
       }
     }
   }
