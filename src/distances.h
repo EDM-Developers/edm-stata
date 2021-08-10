fa8637be2d14f2f9c26d4ff248f18d606955ffe7
pradeep <pradeep@arrayfire.com>
2021-08-10 11:00:03 +0530

    Port potential_neighbors & lp_distances fns of make_prediction to use ArrayFire

--- src/distances.h
+++ src/distances.h
@@ -4,5 +4,7 @@
 
 DistanceIndexPairs lp_distances(int Mp_i, const Options& opts, const Manifold& M, const Manifold& Mp,
                                 std::vector<int> inds);
+DistanceIndexPairs af_lp_distances(int Mp_i, const Options& opts, const Manifold& M, const Manifold& Mp,
+                                   std::vector<int> inds);
 DistanceIndexPairs wasserstein_distances(int Mp_i, const Options& opts, const Manifold& M, const Manifold& Mp,
                                          std::vector<int> inds);
