f461493f81a2ebb588c4e84adbbb6c0a54564469
pradeep <pradeep@arrayfire.com>
2021-09-06 18:35:37 +0530

    Custom kernel for afLPDistances, used by default

---
+++ src/lp_distance.cuh
@@ -0,0 +1,10 @@
+#pragma once
+
+template<typename T>
+void cuLPDistances(char * const valids, T * const distances,
+                   const int npreds, const bool isDistanceMeanAbsoluteError, const bool isPanelMode,
+                   const double idw, const double missingDistance,
+                   const int eacts, const int mnobs,
+                   const T* mData, const int* mPanelIds,
+                   const T* mpData, const int* mpPanelIds,
+                   const char* metricOptions, const cudaStream_t);
