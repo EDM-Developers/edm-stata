9prady9/EDM

Rename MISSING to avoid a clash in Rcpp

src/benchmark.cpp
@@ -244,7 +244,7 @@ static void bm_smap(benchmark::State& state)
 
       double r = ics(0);
       for (int j = 0; j < M.E_actual(); j++) {
-        if (Mp(Mp_i, j) != MISSING) {
+        if (Mp(Mp_i, j) != MISSING_SENTINEL) {
           r += Mp(Mp_i, j) * ics(j + 1);
         }
       }

src/distances.cpp
@@ -34,9 +34,9 @@ DistanceIndexPairs lp_distances(int Mp_i, const Options& opts, const Manifold& M
       // M[i,j] to Mp[Mp_i, j] is opts.missingdistance.
       // However, if the user doesn't specify this, then the entire
       // M[i] to Mp[Mp_i] distance is set as missing.
-      if ((M(i, j) == MISSING) || (Mp(Mp_i, j) == MISSING)) {
+      if ((M(i, j) == MISSING_SENTINEL) || (Mp(Mp_i, j) == MISSING_SENTINEL)) {
         if (opts.missingdistance == 0) {
-          dist_i = MISSING;
+          dist_i = MISSING_SENTINEL;
           break;
         } else {
           dist_ij = opts.missingdistance;
@@ -58,7 +58,7 @@ DistanceIndexPairs lp_distances(int Mp_i, const Options& opts, const Manifold& M
       }
     }
 
-    if (dist_i != 0 && dist_i != MISSING) {
+    if (dist_i != 0 && dist_i != MISSING_SENTINEL) {
       if (opts.distance == Distance::MeanAbsoluteError) {
         dists.push_back(dist_i);
       } else { // Distance::Euclidean
@@ -134,7 +134,7 @@ std::unique_ptr<double[]> wasserstein_cost_matrix(const Manifold& M, const Manif
     double maxData = std::numeric_limits<double>::min();
     double maxTime = 0.0;
     for (int t = 0; t < M_i.cols(); t++) {
-      if (M_i(0, t) != MISSING) {
+      if (M_i(0, t) != MISSING_SENTINEL) {
         if (M_i(0, t) < minData) {
           minData = M_i(0, t);
         }
@@ -142,7 +142,7 @@ std::unique_ptr<double[]> wasserstein_cost_matrix(const Manifold& M, const Manif
           maxData = M_i(0, t);
         }
       }
-      if (M_i(1, t) != MISSING && M_i(1, t) > maxTime) {
+      if (M_i(1, t) != MISSING_SENTINEL && M_i(1, t) > maxTime) {
         maxTime = M_i(1, t);
       }
     }

src/edm.cpp
@@ -227,12 +227,12 @@ Prediction edm_task(const Options opts, const Manifold M, const Manifold Mp, con
   int numCoeffCols = M.E_actual() + 1;
 
   auto ystar = std::make_unique<double[]>(numThetas * numPredictions);
-  std::fill_n(ystar.get(), numThetas * numPredictions, MISSING);
+  std::fill_n(ystar.get(), numThetas * numPredictions, MISSING_SENTINEL);
   Eigen::Map<MatrixXd> ystarView(ystar.get(), numThetas, numPredictions);
 
   // If we're saving the coefficients (i.e. in xmap mode), then we're not running with multiple 'theta' values.
   auto coeffs = std::make_unique<double[]>(numPredictions * numCoeffCols);
-  std::fill_n(coeffs.get(), numPredictions * numCoeffCols, MISSING);
+  std::fill_n(coeffs.get(), numPredictions * numCoeffCols, MISSING_SENTINEL);
   Eigen::Map<MatrixXd> coeffsView(coeffs.get(), numPredictions, numCoeffCols);
 
   auto rc = std::make_unique<retcode[]>(numThetas * numPredictions);
@@ -289,7 +289,7 @@ Prediction edm_task(const Options opts, const Manifold M, const Manifold Mp, con
 
       std::vector<double> y1, y2;
       for (int i = 0; i < Mp.ySize(); i++) {
-        if (Mp.y(i) != MISSING && ystar[i] != MISSING) {
+        if (Mp.y(i) != MISSING_SENTINEL && ystar[i] != MISSING_SENTINEL) {
           y1.push_back(Mp.y(i));
           y2.push_back(ystarView(t, i));
         }
@@ -299,8 +299,8 @@ Prediction edm_task(const Options opts, const Manifold M, const Manifold Mp, con
         stats.mae = mean_absolute_error(y1, y2);
         stats.rho = correlation(y1, y2);
       } else {
-        stats.mae = MISSING;
-        stats.rho = MISSING;
+        stats.mae = MISSING_SENTINEL;
+        stats.rho = MISSING_SENTINEL;
       }
 
       stats.taskNum = opts.taskNum + t;
@@ -614,7 +614,7 @@ void smap_prediction(int Mp_i, int t, const Options& opts, const Manifold& M, co
 
   double r = ics(0);
   for (int j = 0; j < M.E_actual(); j++) {
-    if (Mp(Mp_i, j) != MISSING) {
+    if (Mp(Mp_i, j) != MISSING_SENTINEL) {
       r += Mp(Mp_i, j) * ics(j + 1);
     }
   }
@@ -624,7 +624,7 @@ void smap_prediction(int Mp_i, int t, const Options& opts, const Manifold& M, co
   if (opts.saveSMAPCoeffs && t == opts.thetas.size() - 1) {
     for (int j = 0; j < M.E_actual() + 1; j++) {
       if (ics(j) == 0.) {
-        coeffs(Mp_i, j) = MISSING;
+        coeffs(Mp_i, j) = MISSING_SENTINEL;
       } else {
         coeffs(Mp_i, j) = ics(j);
       }

src/manifold.h
@@ -1,12 +1,16 @@
 #pragma once
 
 /* global variable placeholder for missing values */
-const double MISSING = 1.0e+100;
+
+const double MISSING_SENTINEL = 1.0e+100;
 
 #include <memory>
 #include <vector>
 
-#include <Eigen/Core>
+#define EIGEN_NO_DEBUG
+#define EIGEN_DONT_PARALLELIZE
+#include <Eigen/Dense>
+
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
@@ -64,7 +68,7 @@ public:
     double max = std::numeric_limits<double>::min();
 
     for (int i = 0; i < _nobs * _E_actual; i++) {
-      if (_flat[i] != MISSING) {
+      if (_flat[i] != MISSING_SENTINEL) {
         if (_flat[i] < min) {
           min = _flat[i];
         }
@@ -76,12 +80,12 @@ public:
     return max - min;
   }
 
-  double missing() const { return MISSING; }
+  double missing() const { return MISSING_SENTINEL; }
 
   bool any_missing(int obsNum) const
   {
     for (int j = 0; j < _E_actual; j++) {
-      if (operator()(obsNum, j) == MISSING) {
+      if (operator()(obsNum, j) == MISSING_SENTINEL) {
         return true;
       }
     }
@@ -91,7 +95,7 @@ public:
   bool any_not_missing(int obsNum) const
   {
     for (int j = 0; j < _E_actual; j++) {
-      if (operator()(obsNum, j) != MISSING) {
+      if (operator()(obsNum, j) != MISSING_SENTINEL) {
         return true;
       }
     }
@@ -102,7 +106,7 @@ public:
   {
     int count = 0;
     for (int j = 0; j < _E_actual; j++) {
-      if (operator()(obsNum, j) != MISSING) {
+      if (operator()(obsNum, j) != MISSING_SENTINEL) {
         count += 1;
       }
     }

src/stata.cpp
@@ -130,7 +130,7 @@ std::vector<T> stata_columns(ST_int j0, int numCols = 1)
         }
         if (SF_is_missing(value)) {
           if (std::is_floating_point<T>::value) {
-            value = MISSING;
+            value = MISSING_SENTINEL;
           } else {
             value = 0;
           }
@@ -157,8 +157,8 @@ void write_stata_column(ST_double* data, int len, ST_int j, const std::vector<bo
   for (ST_int i = SF_in1(); i <= SF_in2(); i++) {
     if (SF_ifobs(i)) { // Skip rows according to Stata's 'if'
       if (useEveryRow || filter[r]) {
-        // Convert MISSING back to Stata's missing value
-        ST_double value = (data[obs] == MISSING) ? SV_missval : data[obs];
+        // Convert MISSING_SENTINEL back to Stata's missing value
+        ST_double value = (data[obs] == MISSING_SENTINEL) ? SV_missval : data[obs];
         ST_retcode rc = SF_vstore(j, i, value);
         if (rc) {
           throw std::runtime_error(fmt::format("Cannot write to Stata's variable {}", j));
@@ -187,9 +187,9 @@ void write_stata_columns(double* matrix, int matrixNumRows, int matrixNumCols, S
     if (SF_ifobs(i)) { // Skip rows according to Stata's 'if'
       if (useEveryRow || filter[r]) {
         for (ST_int j = j0; j < j0 + matrixNumCols; j++) {
-          // Convert MISSING back to Stata's missing value
+          // Convert MISSING_SENTINEL back to Stata's missing value
           ST_double value = matrix[obs * matrixNumCols + (j - j0)];
-          if (value == MISSING) {
+          if (value == MISSING_SENTINEL) {
             value = SV_missval;
           }
           ST_retcode rc = SF_vstore(j, i, value);
@@ -654,7 +654,7 @@ ST_retcode save_all_task_results_to_stata(int argc, char* argv[])
       // Save the rho/MAE results if requested (i.e. not for coprediction)
       for (auto& stats : pred.stats) {
         ST_double rho = stats.rho;
-        if (rho == MISSING) {
+        if (rho == MISSING_SENTINEL) {
           rho = SV_missval;
         }
 
@@ -666,7 +666,7 @@ ST_retcode save_all_task_results_to_stata(int argc, char* argv[])
         }
 
         ST_double mae = stats.mae;
-        if (mae == MISSING) {
+        if (mae == MISSING_SENTINEL) {
           mae = SV_missval;
         }
 

src/stats.cpp
@@ -87,17 +87,17 @@ double standard_deviation(const std::vector<double>& vec)
 double default_missing_distance(const std::vector<double>& x)
 {
   const double PI = 3.141592653589793238463;
-  auto xObserved = remove_value(x, MISSING);
+  auto xObserved = remove_value(x, MISSING_SENTINEL);
   double xSD = standard_deviation(xObserved);
   return 2 / sqrt(PI) * xSD;
 }
 
 double default_dt_weight(const std::vector<double>& dts, const std::vector<double>& x)
 {
-  auto xObserved = remove_value(x, MISSING);
+  auto xObserved = remove_value(x, MISSING_SENTINEL);
   double xSD = standard_deviation(xObserved);
 
-  auto dtObserved = remove_value(dts, MISSING);
+  auto dtObserved = remove_value(dts, MISSING_SENTINEL);
   double dtSD = standard_deviation(dtObserved);
 
   if (dtSD == 0.0) {
@@ -113,7 +113,7 @@ Metric guess_appropriate_metric(std::vector<double> data, int targetSample = 100
 
   int sampleSize = 0;
   for (int i = 0; i < data.size() && sampleSize < targetSample; i++) {
-    if (data[i] != MISSING) {
+    if (data[i] != MISSING_SENTINEL) {
       sampleSize += 1;
       uniqueValues.insert(data[i]);
     }

EDM-Developers/edm-stata

src/benchmark.cpp
@@ -244,7 +244,7 @@ static void bm_smap(benchmark::State& state)
 
       double r = ics(0);
       for (int j = 0; j < M.E_actual(); j++) {
-        if (Mp(Mp_i, j) != MISSING) {
+        if (Mp(Mp_i, j) != MISSING_D) {
           r += Mp(Mp_i, j) * ics(j + 1);
         }
       }

src/distances.cpp
@@ -34,9 +34,9 @@ DistanceIndexPairs lp_distances(int Mp_i, const Options& opts, const Manifold& M
       // M[i,j] to Mp[Mp_i, j] is opts.missingdistance.
       // However, if the user doesn't specify this, then the entire
       // M[i] to Mp[Mp_i] distance is set as missing.
-      if ((M(i, j) == MISSING) || (Mp(Mp_i, j) == MISSING)) {
+      if ((M(i, j) == MISSING_D) || (Mp(Mp_i, j) == MISSING_D)) {
         if (opts.missingdistance == 0) {
-          dist_i = MISSING;
+          dist_i = MISSING_D;
           break;
         } else {
           dist_ij = opts.missingdistance;
@@ -58,7 +58,7 @@ DistanceIndexPairs lp_distances(int Mp_i, const Options& opts, const Manifold& M
       }
     }
 
-    if (dist_i != 0 && dist_i != MISSING) {
+    if (dist_i != 0 && dist_i != MISSING_D) {
       if (opts.distance == Distance::MeanAbsoluteError) {
         dists.push_back(dist_i);
       } else { // Distance::Euclidean
@@ -134,7 +134,7 @@ std::unique_ptr<double[]> wasserstein_cost_matrix(const Manifold& M, const Manif
     double maxData = std::numeric_limits<double>::min();
     double maxTime = 0.0;
     for (int t = 0; t < M_i.cols(); t++) {
-      if (M_i(0, t) != MISSING) {
+      if (M_i(0, t) != MISSING_D) {
         if (M_i(0, t) < minData) {
           minData = M_i(0, t);
         }
@@ -142,7 +142,7 @@ std::unique_ptr<double[]> wasserstein_cost_matrix(const Manifold& M, const Manif
           maxData = M_i(0, t);
         }
       }
-      if (M_i(1, t) != MISSING && M_i(1, t) > maxTime) {
+      if (M_i(1, t) != MISSING_D && M_i(1, t) > maxTime) {
         maxTime = M_i(1, t);
       }
     }

src/edm.cpp
@@ -227,12 +227,12 @@ Prediction edm_task(const Options opts, const Manifold M, const Manifold Mp, con
   int numCoeffCols = M.E_actual() + 1;
 
   auto ystar = std::make_unique<double[]>(numThetas * numPredictions);
-  std::fill_n(ystar.get(), numThetas * numPredictions, MISSING);
+  std::fill_n(ystar.get(), numThetas * numPredictions, MISSING_D);
   Eigen::Map<MatrixXd> ystarView(ystar.get(), numThetas, numPredictions);
 
   // If we're saving the coefficients (i.e. in xmap mode), then we're not running with multiple 'theta' values.
   auto coeffs = std::make_unique<double[]>(numPredictions * numCoeffCols);
-  std::fill_n(coeffs.get(), numPredictions * numCoeffCols, MISSING);
+  std::fill_n(coeffs.get(), numPredictions * numCoeffCols, MISSING_D);
   Eigen::Map<MatrixXd> coeffsView(coeffs.get(), numPredictions, numCoeffCols);
 
   auto rc = std::make_unique<retcode[]>(numThetas * numPredictions);
@@ -289,7 +289,7 @@ Prediction edm_task(const Options opts, const Manifold M, const Manifold Mp, con
 
       std::vector<double> y1, y2;
       for (int i = 0; i < Mp.ySize(); i++) {
-        if (Mp.y(i) != MISSING && ystar[i] != MISSING) {
+        if (Mp.y(i) != MISSING_D && ystar[i] != MISSING_D) {
           y1.push_back(Mp.y(i));
           y2.push_back(ystarView(t, i));
         }
@@ -299,8 +299,8 @@ Prediction edm_task(const Options opts, const Manifold M, const Manifold Mp, con
         stats.mae = mean_absolute_error(y1, y2);
         stats.rho = correlation(y1, y2);
       } else {
-        stats.mae = MISSING;
-        stats.rho = MISSING;
+        stats.mae = MISSING_D;
+        stats.rho = MISSING_D;
       }
 
       stats.taskNum = opts.taskNum + t;
@@ -614,7 +614,7 @@ void smap_prediction(int Mp_i, int t, const Options& opts, const Manifold& M, co
 
   double r = ics(0);
   for (int j = 0; j < M.E_actual(); j++) {
-    if (Mp(Mp_i, j) != MISSING) {
+    if (Mp(Mp_i, j) != MISSING_D) {
       r += Mp(Mp_i, j) * ics(j + 1);
     }
   }
@@ -624,7 +624,7 @@ void smap_prediction(int Mp_i, int t, const Options& opts, const Manifold& M, co
   if (opts.saveSMAPCoeffs && t == opts.thetas.size() - 1) {
     for (int j = 0; j < M.E_actual() + 1; j++) {
       if (ics(j) == 0.) {
-        coeffs(Mp_i, j) = MISSING;
+        coeffs(Mp_i, j) = MISSING_D;
       } else {
         coeffs(Mp_i, j) = ics(j);
       }

src/manifold.h
@@ -1,12 +1,16 @@
 #pragma once
 
 /* global variable placeholder for missing values */
-const double MISSING = 1.0e+100;
+
+const double MISSING_D = 1.0e+100;
 
 #include <memory>
 #include <vector>
 
-#include <Eigen/Core>
+#define EIGEN_NO_DEBUG
+#define EIGEN_DONT_PARALLELIZE
+#include <Eigen/Dense>
+
 #include <nlohmann/json.hpp>
 using json = nlohmann::json;
 
@@ -64,7 +68,7 @@ public:
     double max = std::numeric_limits<double>::min();
 
     for (int i = 0; i < _nobs * _E_actual; i++) {
-      if (_flat[i] != MISSING) {
+      if (_flat[i] != MISSING_D) {
         if (_flat[i] < min) {
           min = _flat[i];
         }
@@ -76,12 +80,12 @@ public:
     return max - min;
   }
 
-  double missing() const { return MISSING; }
+  double missing() const { return MISSING_D; }
 
   bool any_missing(int obsNum) const
   {
     for (int j = 0; j < _E_actual; j++) {
-      if (operator()(obsNum, j) == MISSING) {
+      if (operator()(obsNum, j) == MISSING_D) {
         return true;
       }
     }
@@ -91,7 +95,7 @@ public:
   bool any_not_missing(int obsNum) const
   {
     for (int j = 0; j < _E_actual; j++) {
-      if (operator()(obsNum, j) != MISSING) {
+      if (operator()(obsNum, j) != MISSING_D) {
         return true;
       }
     }
@@ -102,7 +106,7 @@ public:
   {
     int count = 0;
     for (int j = 0; j < _E_actual; j++) {
-      if (operator()(obsNum, j) != MISSING) {
+      if (operator()(obsNum, j) != MISSING_D) {
         count += 1;
       }
     }

src/stata.cpp
@@ -130,7 +130,7 @@ std::vector<T> stata_columns(ST_int j0, int numCols = 1)
         }
         if (SF_is_missing(value)) {
           if (std::is_floating_point<T>::value) {
-            value = MISSING;
+            value = MISSING_D;
           } else {
             value = 0;
           }
@@ -157,8 +157,8 @@ void write_stata_column(ST_double* data, int len, ST_int j, const std::vector<bo
   for (ST_int i = SF_in1(); i <= SF_in2(); i++) {
     if (SF_ifobs(i)) { // Skip rows according to Stata's 'if'
       if (useEveryRow || filter[r]) {
-        // Convert MISSING back to Stata's missing value
-        ST_double value = (data[obs] == MISSING) ? SV_missval : data[obs];
+        // Convert MISSING_D back to Stata's missing value
+        ST_double value = (data[obs] == MISSING_D) ? SV_missval : data[obs];
         ST_retcode rc = SF_vstore(j, i, value);
         if (rc) {
           throw std::runtime_error(fmt::format("Cannot write to Stata's variable {}", j));
@@ -187,9 +187,9 @@ void write_stata_columns(double* matrix, int matrixNumRows, int matrixNumCols, S
     if (SF_ifobs(i)) { // Skip rows according to Stata's 'if'
       if (useEveryRow || filter[r]) {
         for (ST_int j = j0; j < j0 + matrixNumCols; j++) {
-          // Convert MISSING back to Stata's missing value
+          // Convert MISSING_D back to Stata's missing value
           ST_double value = matrix[obs * matrixNumCols + (j - j0)];
-          if (value == MISSING) {
+          if (value == MISSING_D) {
             value = SV_missval;
           }
           ST_retcode rc = SF_vstore(j, i, value);
@@ -654,7 +654,7 @@ ST_retcode save_all_task_results_to_stata(int argc, char* argv[])
       // Save the rho/MAE results if requested (i.e. not for coprediction)
       for (auto& stats : pred.stats) {
         ST_double rho = stats.rho;
-        if (rho == MISSING) {
+        if (rho == MISSING_D) {
           rho = SV_missval;
         }
 
@@ -666,7 +666,7 @@ ST_retcode save_all_task_results_to_stata(int argc, char* argv[])
         }
 
         ST_double mae = stats.mae;
-        if (mae == MISSING) {
+        if (mae == MISSING_D) {
           mae = SV_missval;
         }
 

src/stats.cpp
@@ -87,17 +87,17 @@ double standard_deviation(const std::vector<double>& vec)
 double default_missing_distance(const std::vector<double>& x)
 {
   const double PI = 3.141592653589793238463;
-  auto xObserved = remove_value(x, MISSING);
+  auto xObserved = remove_value(x, MISSING_D);
   double xSD = standard_deviation(xObserved);
   return 2 / sqrt(PI) * xSD;
 }
 
 double default_dt_weight(const std::vector<double>& dts, const std::vector<double>& x)
 {
-  auto xObserved = remove_value(x, MISSING);
+  auto xObserved = remove_value(x, MISSING_D);
   double xSD = standard_deviation(xObserved);
 
-  auto dtObserved = remove_value(dts, MISSING);
+  auto dtObserved = remove_value(dts, MISSING_D);
   double dtSD = standard_deviation(dtObserved);
 
   if (dtSD == 0.0) {
@@ -113,7 +113,7 @@ Metric guess_appropriate_metric(std::vector<double> data, int targetSample = 100
 
   int sampleSize = 0;
   for (int i = 0; i < data.size() && sampleSize < targetSample; i++) {
-    if (data[i] != MISSING) {
+    if (data[i] != MISSING_D) {
       sampleSize += 1;
       uniqueValues.insert(data[i]);
     }